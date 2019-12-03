720ca078b4380fe3000a79676b08969f01c6badd
Anshuk Aggarwal <aggar@LAPTOP-OSKI3RTS>
2019-12-03 18:04:16 -0500

    Added carrers page with JS

--- scripts/script.js
+++ scripts/script.js
@@ -5,4 +5,9 @@ jQuery(document).ready(function(){
 		$('.try').hide();
 		$(this).next('.try').show();
 	});//end of click
+	$('.desc').hide();
+	$('tr').click(function(){
+		$('.desc').hide();
+		$(this).find('td.desc').show();
+	});//end of click table
 });//end of onlaod

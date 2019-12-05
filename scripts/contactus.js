cd6957a2749f5c636f21dd0c831664d2484ce01b
rosezhan <54869251+rosezhan@users.noreply.github.com>
2019-12-05 16:39:30 -0500

    js update

--- scripts/contactus.js
+++ scripts/contactus.js
@@ -4,17 +4,25 @@ window.onload = function() {
 
 var firstName = document.getElementById("customer-fname");
 var lastName = document.getLementById("customer-lname");
+var fName = /^[a-zA-Z]$/;
+var lName = /^[a-zA-z]$/;
+
     
 var contactForm = document.forms[0];
 contactForm.onsubmit = processForm;
     
 function processForm(){
-    var fName = /^[a-zA-Z]$/;
-    if (fName.test(firstName.value)) {
-        
+    if (!fName.test(firstName.value)) {
+        firstName.style.background="red";
+    } else {
+        firstName.style.background="white";
     }
-    var lName = /^[a-zA-z]$/;
     
+    if (!lName.test(lastName.value)) {
+        lastName.style.background="red";
+    } else {
+        lastName.style.background="white";
+    }
     
     }//process form
 }//window onload

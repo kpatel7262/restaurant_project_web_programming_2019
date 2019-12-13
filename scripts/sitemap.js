6e169a40df8d0e9737dd3ac31a7086ff2ff7eab9
krinapatel440@gmail.com <krinapatel440@gmail.com>
2019-12-13 12:45:34 -0500

    upadated site map and contest page

--- scripts/sitemap.js
+++ scripts/sitemap.js
@@ -1,40 +1,45 @@
 
 
-
 jQuery(document).ready(function(){
-	//alert("welcome to the sitemap of RACCO restaurent");
+	alert("welcome to the sitemap of RACCO restaurent click on the Pages of Sitemap to see the site map");
 	$("#subMenu").hide();
-	//$("#sitemap").hide();
+	$("#sitemap").hide();
 	$("#eventList").hide();
 	
 	//alert(document.getElementById("menu"));
 	$("#mainMenu").click(showFunction);
 	$("#eventMenu").click(showFunction2);
-	//$("#sitemapHeader").click(sitemap);
+	$("#sitemapHeader").click(sitemap);
 	$("a").click(links);
-	$( "a" ).hover(links2);
 	
 	
 	function showFunction(){
 		//alert("This is the submenu of Menu");
 		$("#subMenu").toggle(3000);
 	}
-	// function sitemap(){
-		// //alert("This is the sitemap of RACCO restaurent");
-		// $("#sitemap").toggle(3000);
-	// }
+	function sitemap(){
+		//alert("This is the sitemap of RACCO restaurent");
+		$("#sitemap").toggle(3000);
+	}
 	function showFunction2(){
 		//alert("This is the submenu of Events");
 		$("#eventList").toggle(3000);
 	}
 	function links(){
-    $("a").css("color","#CB2D5A");
-	}
-	function links2(){
-  	$("a").css("color","rgb(195, 144, 144)")
+    $("a").css("color","white");
 	}
 	
-	
-});
+	$("a").mouseover(function()
+	{
+	$(this).css("border-bottom", "1px solid #fff").css( "color", "#CB2D5A");
+	});
+
+	$("a").mouseleave(function()
+	{
+	  $(this).css("border-bottom", "none").css("color", "white");
+	});
+	});
+
+
 
 	

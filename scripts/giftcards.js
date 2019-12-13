ce72d6e93c85b6cc7f26b3b7b0546b16b5d27acd
kpatel7262 <55475268+kpatel7262@users.noreply.github.com>
2019-12-13 09:08:28 -0500

    giftcards and location final js

---
+++ scripts/giftcards.js
@@ -0,0 +1,25 @@
+window.onload = pageReady;
+function pageReady(){
+
+	var bronze = document.getElementById("b_subscribe");
+	var silver = document.getElementById("s_subscribe");
+	var gold = document.getElementById("g_subscribe");
+
+	bronze.onclick = alert1;
+	silver.onclick = alert2;
+	gold.onclick = alert3;
+
+	function alert1() {
+        alert("Wohooo! You have successfully subscribe to BRONZE card");
+      }
+
+      function alert2() {
+        alert("Wohooo! You have successfully subscribe to SILVER card");
+      }
+
+      function alert3() {
+        alert("Wohooo! You have successfully subscribe to GOLD card");
+      }
+}
+        
+  
---
+++ scripts/locations.js
@@ -0,0 +1,13 @@
+window.onload = pageReady;
+function pageReady(){
+
+var map2= document.getElementById("location2_map");
+
+map2.onclick = open_window;
+
+function open_window(){
+  window.open("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d92362.55838906797!2d-79.63024604171181!3d43.66210725554357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b4835e6b98679%3A0x5ca4f10b097c8c4f!2sHumber%20College%20Lakeshore%20Campus%3A%20Building%20A!5e0!3m2!1sen!2sca!4v1576038396398!5m2!1sen!2sca");
+}
+
+}
+

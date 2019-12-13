4533b9e763f267594414846d0d34fc5949c6e042
krinapatel440@gmail.com <krinapatel440@gmail.com>
2019-12-13 12:15:48 -0500

    site map updated

--- scripts/sitemap.js
+++ scripts/sitemap.js
@@ -4,13 +4,13 @@
 jQuery(document).ready(function(){
 	//alert("welcome to the sitemap of RACCO restaurent");
 	$("#subMenu").hide();
-	$("#sitemap").hide();
+	//$("#sitemap").hide();
 	$("#eventList").hide();
 	
 	//alert(document.getElementById("menu"));
 	$("#mainMenu").click(showFunction);
 	$("#eventMenu").click(showFunction2);
-	$("#sitemapHeader").click(sitemap);
+	//$("#sitemapHeader").click(sitemap);
 	$("a").click(links);
 	$( "a" ).hover(links2);
 	
@@ -19,10 +19,10 @@ jQuery(document).ready(function(){
 		//alert("This is the submenu of Menu");
 		$("#subMenu").toggle(3000);
 	}
-	function sitemap(){
-		//alert("This is the sitemap of RACCO restaurent");
-		$("#sitemap").toggle(3000);
-	}
+	// function sitemap(){
+		// //alert("This is the sitemap of RACCO restaurent");
+		// $("#sitemap").toggle(3000);
+	// }
 	function showFunction2(){
 		//alert("This is the submenu of Events");
 		$("#eventList").toggle(3000);
